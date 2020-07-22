d0e81be2ae39a989fcd44f55a3a6a7a60e013c9f
arogya247 <41734737+arogya247@users.noreply.github.com>
2020-07-22 22:39:45 +0530

    Create PermutationWithCaseChange.py

---
+++ PermutationWithCaseChange.py
@@ -0,0 +1,18 @@
+# permutation with case change
+
+
+def solve(ip,op):
+    if len(ip)==0:
+        print(op)
+        return 
+    op1 = op
+    op2 = op
+    op1+=ip[0]
+    op2+=ip[0].upper()
+    ip = ip[1:]
+    solve(ip,op1)
+    solve(ip,op2)
+    return
+
+
+solve("ab","")
